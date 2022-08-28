f5e1339e063d341ef6f5baf8fc082c62417b3b6f
shimwell <mail@jshimwell.com>
2022-08-29 00:30:37 +0100

    inital refactor

--- examples/example_plot_source_energy.py
+++ examples/example_plot_source_energy.py
@@ -1,16 +1,18 @@
-import openmc_source_plotter as osp
+from openmc_source_plotter import SettingsWithSourcePlotting
 import openmc
 
-# initialises a new source object
+# initialise a new source object
 my_source = openmc.Source()
 
 # sets the energy distribution to a Muir distribution neutrons
 my_source.energy = openmc.stats.Muir(e0=14080000.0, m_rat=5.0, kt=20000.0)
 
+# initialise a new settings object
+my_settings = SettingsWithSourcePlotting()
+
+my_settings.source = my_source
+
 # plots the particle energy distribution
-plot = osp.plot_source_energy(
-    source=my_source,
-    n_samples=10000,
-)
+plot = my_settings.plot_source_energy(n_samples=10000)
 
 plot.show()
--- openmc_source_plotter/core.py
+++ openmc_source_plotter/core.py
@@ -9,60 +9,61 @@ import openmc
 import openmc.lib
 import plotly.graph_objects as go
 
-
-def sample_initial_particles(
-    source: openmc.source, n_samples: int = 1000, prn_seed: int = None
-):
-
-    settings = openmc.Settings()
-    settings.particles = 1
-    settings.batches = 1
-    settings.source = source
-    settings.export_to_xml()
-
-    materials = openmc.Materials()
-    materials.export_to_xml()
-
-    sph = openmc.Sphere(r=9999999999, boundary_type="vacuum")
-    cell = openmc.Cell(region=-sph)
-    geometry = openmc.Geometry([cell])
-
-    geometry.export_to_xml()
-
-    openmc.lib.init()
-    particles = openmc.lib.sample_external_source(
-        n_samples=n_samples, prn_seed=prn_seed
-    )
-    openmc.lib.finalize()
-    return particles
-
-
-def plot_source_energy(
-    source: Union[openmc.Source, List[openmc.Source]],
-    n_samples: int = 2000,
-    prn_seed: int = 1,
-    energy_bins: Union[str, np.array] = "auto",
-):
-    """makes a plot of the initial creation postions of an OpenMC source(s)
-
-    Args:
-        source: The openmc.Source object or list of openmc.Source objects to
-            plot.
-        n_samples: The number of source samples to obtain.
-        prn_seed: The pseudorandom number seed
-        energy_bins: Defaults to 'auto' which uses inbuilt auto binning in
-            Numpy bins can also be manually set by passing in a numpy array of
-            bin edges.
+class SettingsWithSourcePlotting(openmc.Settings):
+    r"""Extends the openmc.Settings to add source plotting methods for energy
+    direction and space.
     """
+    
 
-    figure = go.Figure()
+    def sample_initial_particles(
+        self, n_samples: int = 1000, prn_seed: int = None
+    ):
 
-    if isinstance(source, openmc.Source):
-        source = [source]
+        settings = openmc.Settings()
+        settings.particles = 1
+        settings.batches = 1
+        settings.source = self.source
+        settings.export_to_xml()
 
-    for single_source in source:
+        materials = openmc.Materials()
+        materials.export_to_xml()
 
-        data = sample_initial_particles(single_source, n_samples, prn_seed)
+        sph = openmc.Sphere(r=9999999999, boundary_type="vacuum")
+        cell = openmc.Cell(region=-sph)
+        geometry = openmc.Geometry([cell])
+
+        geometry.export_to_xml()
+
+        openmc.lib.init()
+        particles = openmc.lib.sample_external_source(
+            n_samples=n_samples, prn_seed=prn_seed
+        )
+        openmc.lib.finalize()
+
+        return particles
+
+
+    def plot_source_energy(
+        self,
+        n_samples: int = 2000,
+        prn_seed: int = 1,
+        energy_bins: Union[str, np.array] = "auto",
+    ):
+        """makes a plot of the initial creation postions of an OpenMC source(s)
+
+        Args:
+            source: The openmc.Source object or list of openmc.Source objects to
+                plot.
+            n_samples: The number of source samples to obtain.
+            prn_seed: The pseudorandom number seed
+            energy_bins: Defaults to 'auto' which uses inbuilt auto binning in
+                Numpy bins can also be manually set by passing in a numpy array of
+                bin edges.
+        """
+
+        figure = go.Figure()
+
+        data = self.sample_initial_particles(n_samples, prn_seed)
 
         e_values = [particle.E for particle in data]
 
@@ -79,103 +80,103 @@ def plot_source_energy(
             )
         )
 
-    figure.update_layout(
-        title="Particle energy",
-        xaxis={"title": "Energy (eV)"},
-        yaxis={"title": "Probability"},
-        showlegend=True,
-    )
+        figure.update_layout(
+            title="Particle energy",
+            xaxis={"title": "Energy (eV)"},
+            yaxis={"title": "Probability"},
+            showlegend=True,
+        )
 
-    return figure
+        return figure
 
 
-def plot_source_position(
-    source: Union[openmc.Source, List[openmc.Source]],
-    n_samples: int = 2000,
-    prn_seed: int = 1,
-):
-    """makes a plot of the initial creation postions of an OpenMC source(s)
+    def plot_source_position(
+        source: Union[openmc.Source, List[openmc.Source]],
+        n_samples: int = 2000,
+        prn_seed: int = 1,
+    ):
+        """makes a plot of the initial creation postions of an OpenMC source(s)
 
-    Args:
-        source: The openmc.Source object or list of openmc.Source objects to
-            plot.
-        n_samples: The number of source samples to obtain.
-        prn_seed: The pseudorandom number seed
-    """
+        Args:
+            source: The openmc.Source object or list of openmc.Source objects to
+                plot.
+            n_samples: The number of source samples to obtain.
+            prn_seed: The pseudorandom number seed
+        """
 
-    figure = go.Figure()
+        figure = go.Figure()
 
-    if isinstance(source, openmc.Source):
-        source = [source]
+        if isinstance(source, openmc.Source):
+            source = [source]
 
-    for single_source in source:
+        for single_source in source:
 
-        data = sample_initial_particles(single_source, n_samples, prn_seed)
+            data = self.sample_initial_particles(single_source, n_samples, prn_seed)
 
-        text = ["Energy = " + str(particle.E) + " eV" for particle in data]
+            text = ["Energy = " + str(particle.E) + " eV" for particle in data]
 
-        figure.add_trace(
-            go.Scatter3d(
-                x=[particle.r[0] for particle in data],
-                y=[particle.r[1] for particle in data],
-                z=[particle.r[2] for particle in data],
-                hovertext=text,
-                text=text,
-                mode="markers",
-                marker={
-                    "size": 2,
-                    # "color": data.E,
-                },
+            figure.add_trace(
+                go.Scatter3d(
+                    x=[particle.r[0] for particle in data],
+                    y=[particle.r[1] for particle in data],
+                    z=[particle.r[2] for particle in data],
+                    hovertext=text,
+                    text=text,
+                    mode="markers",
+                    marker={
+                        "size": 2,
+                        # "color": data.E,
+                    },
+                )
             )
-        )
-    title = "Particle production coordinates coloured by energy"
-    figure.update_layout(title=title)
+        title = "Particle production coordinates coloured by energy"
+        figure.update_layout(title=title)
 
-    return figure
+        return figure
 
 
-def plot_source_direction(
-    source: Union[openmc.Source, List[openmc.Source]],
-    n_samples: int = 2000,
-    prn_seed: int = 1,
-):
-    """makes a plot of the initial creation postions of an OpenMC source(s)
+    def plot_source_direction(
+        source: Union[openmc.Source, List[openmc.Source]],
+        n_samples: int = 2000,
+        prn_seed: int = 1,
+    ):
+        """makes a plot of the initial creation postions of an OpenMC source(s)
 
-    Args:
-        source: The openmc.Source object or list of openmc.Source objects to
-            plot.
-        n_samples: The number of source samples to obtain.
-        prn_seed: The pseudorandom number seed
-    """
-    figure = go.Figure()
+        Args:
+            source: The openmc.Source object or list of openmc.Source objects to
+                plot.
+            n_samples: The number of source samples to obtain.
+            prn_seed: The pseudorandom number seed
+        """
+        figure = go.Figure()
 
-    if isinstance(source, openmc.Source):
-        source = [source]
+        if isinstance(source, openmc.Source):
+            source = [source]
 
-    for single_source in source:
-        data = sample_initial_particles(single_source, n_samples, prn_seed)
+        for single_source in source:
+            data = self.sample_initial_particles(single_source, n_samples, prn_seed)
 
-        figure.add_trace(
-            {
-                "type": "cone",
-                "cauto": False,
-                "x": [particle.r[0] for particle in data],
-                "y": [particle.r[1] for particle in data],
-                "z": [particle.r[2] for particle in data],
-                "u": [particle.u[0] for particle in data],
-                "v": [particle.u[1] for particle in data],
-                "w": [particle.u[2] for particle in data],
-                "cmin": 0,
-                "cmax": 1,
-                "anchor": "tail",
-                "colorscale": "Viridis",
-                "hoverinfo": "u+v+w+norm",
-                "sizemode": "absolute",
-                "sizeref": 30,
-                "showscale": False,
-            }
-        )
+            figure.add_trace(
+                {
+                    "type": "cone",
+                    "cauto": False,
+                    "x": [particle.r[0] for particle in data],
+                    "y": [particle.r[1] for particle in data],
+                    "z": [particle.r[2] for particle in data],
+                    "u": [particle.u[0] for particle in data],
+                    "v": [particle.u[1] for particle in data],
+                    "w": [particle.u[2] for particle in data],
+                    "cmin": 0,
+                    "cmax": 1,
+                    "anchor": "tail",
+                    "colorscale": "Viridis",
+                    "hoverinfo": "u+v+w+norm",
+                    "sizemode": "absolute",
+                    "sizeref": 30,
+                    "showscale": False,
+                }
+            )
 
-    figure.update_layout(title="Particle initial directions")
+        figure.update_layout(title="Particle initial directions")
 
-    return figure
+        return figure
