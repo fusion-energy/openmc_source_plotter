add1e97cec4853715ad4962cbd24c46730f0c73b
Jonathan Shimwell <mail@jshimwell.com>
2022-09-28 17:46:37 +0100

    added labels

--- openmc_source_plotter/material.py
+++ openmc_source_plotter/material.py
@@ -14,31 +14,23 @@ class Material(openmc.Material):
         energies_to_label = []
         labels=[]
         atoms = self.get_nuclide_atoms()
-        dists = []
-        probs = []
         for nuc, num_atoms in atoms.items():
+            dists = []
+            probs = []
+            print(nuc)
             source_per_atom = openmc.data.decay_photon_energy(nuc)
             if source_per_atom is not None:
-                # dists.append(source_per_atom)
-                # probs.append(num_atoms)
-                if num_atoms>threshold:
-                    energies_to_label.append(source_per_atom.x)
-                    labels.append(str(source_per_atom.p))
-        # return openmc.data.combine_distributions(dists, probs)
-
-        # ps = self.decay_photon_energy.p
-        # xs = self.decay_photon_energy.x
-
-        # for x,p in zip(xs,ps):
-        #     print(p)
-        #     if p > threshold:
-
-        # print(energies_to_label)
-        # print(labels)
+                dists.append(source_per_atom)
+                probs.append(num_atoms)
+                combo = openmc.data.combine_distributions(dists, probs)
+                for p,x in zip(combo.p, combo.x):
+                    if p>threshold:
+                        print('   ',p,x)
+                        energies_to_label.append(x)
+                        labels.append(nuc)
 
         lineid_plot.plot_line_ids(self.decay_photon_energy.x, self.decay_photon_energy.p, energies_to_label, labels)
-# my_material.nuclides[0]
-# NuclideTuple(name='Xe135', percent=1, percent_type='ao')
+
 
         
         # plt.plot()
