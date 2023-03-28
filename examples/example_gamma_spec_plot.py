6bb64b0303a157b928b88171a83deac5efe26268
Jonathan Shimwell <mail@jshimwell.com>
2023-03-28 16:05:20 +0100

    updated readme with gammas spec image

--- examples/example_gamma_spec_plot.py
+++ examples/example_gamma_spec_plot.py
@@ -1,5 +1,4 @@
 import openmc
-import openmc.deplete
 import openmc_source_plotter  # adds plot_gamma_emission plot to materials
 
 
@@ -10,8 +9,9 @@ my_material.add_nuclide("Xe135", 1e-12)
 my_material.add_nuclide("U235", 1)
 my_material.add_nuclide("U238", 1)
 my_material.add_nuclide("Co60", 1e-9)
-my_material.volume = 1
+my_material.volume = 1  # must be set so number of atoms can be found
 
+# adds labels to the most active 3 gamma energies
 plt = my_material.plot_gamma_emission(label_top=3)
 plt.xscale("log")  # modify axis from default settings
 plt.savefig("gamma_spec.png")
